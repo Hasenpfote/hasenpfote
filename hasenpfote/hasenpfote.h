ac065e746ffaf8d8f9a3343aa905d269a5ac9a07
Hasenpfote <hasenpfote36@gmail.com>
2016-07-08 13:34:28 +0900

    不要なヘッダを削除.

--- hasenpfote/hasenpfote.h
+++ hasenpfote/hasenpfote.h
@@ -6,12 +6,6 @@
 */
 #pragma once
 
-#include "math/constants.h"
-#include "math/utility.h"
-#include "math/vector2.h"
-#include "math/vector3.h"
-#include "math/cmatrix4.h"
-#include "math/rmatrix4.h"
-#include "math/quaternion.h"
-#include "math/axis_angle.h"
-#include "quantization/quantization.h"
+#ifdef _MSC_VER
+ #pragma comment(lib, "hasenpfote.lib")
+#endif
