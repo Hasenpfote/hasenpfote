a3e33a5c7480b1f36996135884da3491a2f6ffb7
Hasenpfote <hasenpfote36@gmail.com>
2016-11-10 21:34:56 +0900

    g++ -Wfloat-equal 対応.

--- sources/math/utility.cpp
+++ sources/math/utility.cpp
@@ -10,8 +10,16 @@ bool almost_equals(float a, float b, std::uint32_t max_ulps)
 {
     // Infinity check
     // FLT_MAX 近辺での比較を行わないよう単純比較に切り替える
-    if(std::isinf(a) || std::isinf(b))
+    if(std::isinf(a) || std::isinf(b)){
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+#endif
         return a == b;
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+    }
     // NaN check
     // max_ulps が NaN との比較を行わないようにする
     // NaN - FLT_MAX - 1 でも代替可
@@ -20,8 +28,16 @@ bool almost_equals(float a, float b, std::uint32_t max_ulps)
     // Sign check
     auto ia = *reinterpret_cast<std::int32_t*>(&a);
     auto ib = *reinterpret_cast<std::int32_t*>(&b);
-    if((ia & 0x80000000) != (ib & 0x80000000))
+    if((ia & 0x80000000) != (ib & 0x80000000)){
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+#endif
         return a == b;
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+    }
     // 辞書順に並べ替え比較
     if(ia < 0)
         ia = 0x80000000 - ia;
@@ -37,8 +53,16 @@ bool almost_equals(double a, double b, std::uint64_t max_ulps)
 {
     // Infinity check
     // DBL_MAX 近辺での比較を行わないよう単純比較に切り替える
-    if(std::isinf(a) || std::isinf(b))
+    if(std::isinf(a) || std::isinf(b)){
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+#endif
         return a == b;
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+    }
     // NaN check
     // max_ulps が NaN との比較を行わないようにする
     // NaN - DBL_MAX - 1 でも代替可
@@ -47,8 +71,16 @@ bool almost_equals(double a, double b, std::uint64_t max_ulps)
     // Sign check
     auto ia = *reinterpret_cast<std::int64_t*>(&a);
     auto ib = *reinterpret_cast<std::int64_t*>(&b);
-    if((ia & 0x8000000000000000L) != (ib & 0x8000000000000000L))
+    if((ia & 0x8000000000000000L) != (ib & 0x8000000000000000L)){
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+#endif
         return a == b;
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+    }
     // 辞書順に並べ替え比較
     if(ia < 0)
         ia = 0x8000000000000000L - ia;
