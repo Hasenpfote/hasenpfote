2183bc4c4d440946d884cc38d68f632f549dc1c9
Hasenpfote <hasenpfote36@gmail.com>
2016-11-10 20:12:27 +0900

    filesystem 関連の修正.

--- sources/log/file_appender.cpp
+++ sources/log/file_appender.cpp
@@ -2,10 +2,19 @@
 
 namespace hasenpfote{ namespace log{
 
+FileAppender::FileAppender(const std::string& filepath)
+    : ofs(std::make_unique<std::ofstream>(filepath))
+{
+}
+
+#if (__cplusplus > 201402L) || (defined(_MSC_VER) && (_MSC_VER > 1900))
+#error Function not implemented.
+#elif defined(_MSC_VER) && (_MSC_VER == 1900)
 FileAppender::FileAppender(const std::tr2::sys::path& filepath)
     : ofs(std::make_unique<std::ofstream>(filepath))
 {
 }
+#endif
 
 void FileAppender::Write(const std::string& buffer)
 {
--- sources/log/rolling_file_appender.cpp
+++ sources/log/rolling_file_appender.cpp
@@ -4,6 +4,9 @@
 
 namespace hasenpfote{ namespace log{
 
+#if (__cplusplus > 201402L) || defined(_MSC_VER) && (_MSC_VER > 1900)
+#error Function not implemented.
+#elif defined(_MSC_VER) && (_MSC_VER == 1900)
 static std::tr2::sys::path BuildFilePath(const std::tr2::sys::path& filepath, int number)
 {
     auto new_filepath(filepath);
@@ -14,6 +17,41 @@ static std::tr2::sys::path BuildFilePath(const std::tr2::sys::path& filepath, in
     new_filepath.replace_extension(filepath.extension());
     return new_filepath;
 }
+
+static bool ExistsFile(const std::tr2::sys::path& filepath)
+{
+    return std::tr2::sys::exists(filepath);
+}
+
+static bool RenameFile(const std::tr2::sys::path& old_filepath, const std::tr2::sys::path& new_filepath)
+{
+    std::error_code ret;
+    std::tr2::sys::rename(old_filepath, new_filepath, ret);
+    return (ret)? false : true;
+}
+#else
+static std::string BuildFilePath(const std::string& filepath, int number)
+{
+    auto extension = "." + std::to_string(number);
+    const auto n = filepath.rfind(".");
+    if(n != std::string::npos){
+        extension.append(filepath.substr(n));
+    }
+    return filepath.substr(0, n) + extension;
+}
+
+static bool ExistsFile(const std::string& filepath)
+{
+    std::ifstream infile(filepath);
+    return infile.good();
+}
+
+static bool RenameFile(const std::string& old_filepath, const std::string& new_filepath)
+{
+    auto ret = std::rename(old_filepath.c_str(), new_filepath.c_str());
+    return (ret)? false : true;
+}
+#endif
 
 static std::streampos GetNextPosition(std::ofstream& ofs, const std::string& buffer)
 {
@@ -26,11 +64,21 @@ static std::streampos GetNextPosition(std::ofstream& ofs, const std::string& buf
     return pos;
 }
 
+RollingFileAppender::RollingFileAppender(const std::string& filepath, int max_files, std::size_t max_file_size)
+    : ofs(std::make_unique<std::ofstream>()), filepath(filepath), max_files(max_files), max_file_size(max_file_size)
+{
+    assert(max_files > 0);
+}
+
+#if (__cplusplus > 201402L) || defined(_MSC_VER) && (_MSC_VER > 1900)
+#error Function not implemented.
+#elif defined(_MSC_VER) && (_MSC_VER == 1900)
 RollingFileAppender::RollingFileAppender(const std::tr2::sys::path& filepath, int max_files, std::size_t max_file_size)
     : ofs(std::make_unique<std::ofstream>()), filepath(filepath), max_files(max_files), max_file_size(max_file_size)
 {
     assert(max_files > 0);
 }
+#endif
 
 void RollingFileAppender::Write(const std::string& buffer)
 {
@@ -41,12 +89,12 @@ void RollingFileAppender::Write(const std::string& buffer)
     if(max_files > 1 && (GetNextPosition(*ofs, buffer) >= max_file_size)){
         ofs->close();
         // rolling files.
-        std::error_code ret;
         for(auto i = max_files - 2; i >= 0; i--){
             auto current_filepath = (i > 0)? BuildFilePath(filepath, i) : filepath;
-            if(std::tr2::sys::exists(current_filepath)){
-                auto next_filepath = BuildFilePath(filepath, i+1);
-                std::tr2::sys::rename(current_filepath, next_filepath, ret);
+            if(ExistsFile(current_filepath)){
+                auto next_filepath = BuildFilePath(filepath, i + 1);
+                auto ret = RenameFile(current_filepath, next_filepath);
+                (void)ret;
             }
         }
         ofs->open(filepath);
