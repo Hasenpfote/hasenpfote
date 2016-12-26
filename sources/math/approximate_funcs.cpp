54dad7f6df05ac3cc7b4f54010b7ab034d7e69e5
Hasenpfote <hasenpfote36@gmail.com>
2016-12-26 15:45:51 +0900

    新規作成.

---
+++ sources/math/approximate_funcs.cpp
@@ -0,0 +1,19 @@
+﻿#include "approximate_funcs.h"
+
+namespace hasenpfote{ namespace math{
+
+float sinc(float x)
+{
+    const float x2 = x * x;
+    const float x4 = x2 * x2;
+    return 1.0f - x2 / 6.0f + x4 / 120.0f - (x2 * x4) / 5040.0f;
+}
+
+float rcp_sinc(float x)
+{
+    const float x2 = x * x;
+    const float x4 = x2 * x2;
+    return 1.0f + x2 / 6.0f + 7.0f * x4 / 360.0f + 31.0f * (x2 * x4) / 15120.0f;
+}
+
+}}
